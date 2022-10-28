03e195a2dc2764a6ec0646255fdf34cf3fa79cf6
MUKIL KUMAR <42886243+bukil@users.noreply.github.com>
2022-10-28 11:10:54 +0530

    Latest
    Latest

--- pages/index.js
+++ pages/index.js
@@ -4,32 +4,17 @@ import {
   Container,
   Heading,
   Box,
-  SimpleGrid,
-  Button,
-  List,
   ListItem,
-  useColorModeValue,
-  chakra,
-  UnorderedList,
-  Stack,
-  ListIcon
-
 } from '@chakra-ui/react'
 
-import { ChevronRightIcon } from '@chakra-ui/icons'
 import Paragraph from '../components/paragraph'
-import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { Grid, GridItem } from '@chakra-ui/react'
-import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub} from 'react-icons/io5'
-import thumbYouTube from '../public/images/links/youtube.png'
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
 import Image from 'next/image'
 import { Divider, Progress } from '@chakra-ui/react'
 import { Flex, Spacer } from '@chakra-ui/react'
 
-import {SmallAddIcon} from '@chakra-ui/icons'
 
 const ProfileImage = chakra(Image, {
   shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
